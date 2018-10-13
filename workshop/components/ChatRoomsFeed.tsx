fc29dbd034ff830743eb9fdf5b3a11b75264e818
Daler Asrorov <asrorids@gmail.com>
2018-10-13 04:46:32 -0700

    Remove unused creat chatroom action in Feed

--- workshop/components/ChatRoomsFeed.tsx
+++ workshop/components/ChatRoomsFeed.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { StyleSheet, TextInput, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Button } from 'react-native';
 import { NavigationParams } from 'react-navigation';
-import { SECONDARY_COLOR, PRIMARY_COLOR } from '../colors';
+import { SECONDARY_COLOR } from '../colors';
 
 interface ChatRoomsFeedProps {
   username: string;
-  onCreateChatRoom: (chatroom: ChatRoomProps) => any;
   navigation: NavigationParams;
 }
 
@@ -13,18 +12,6 @@ export default class ChatRoomsFeed extends React.PureComponent<
   ChatRoomsFeedProps,
   {}
 > {
-  private createNewChatRoom = () => {
-    const { onCreateChatRoom, username } = this.props;
-
-    // ToDo: create a modal with a form
-    // that passes these parameters
-    // onCreateChatRoom({
-    //   id: `${username}'s Room`,
-    //   owner: username,
-    //   themeColor: '#104E8B'
-    // });
-  };
-
   render() {
     const { navigation } = this.props;
 
