da80bd049d1ee1e2ae8b989a17c00b2a329005f4
Daler Asrorov <asrorids@gmail.com>
2018-10-13 04:40:11 -0700

    Create chatroom via UI with color selection

--- workshop/components/CreateRoomModalScreen.tsx
+++ workshop/components/CreateRoomModalScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button, TextInput, View, StyleSheet } from 'react-native';
+import { ColorPicker } from 'react-native-color-picker';
 import { CustomInput, CustomButton } from './CustomInputs';
 import { PRIMARY_COLOR } from '../colors';
 
@@ -28,32 +29,48 @@ export default class CreateRoomModalScreen extends React.Component<
     });
   };
 
+  private handleColorSelect = (colorHex: string) => {
+    this.setState({
+      themeColor: colorHex
+    });
+  };
+
   private handleRoomCreate = () => {
     const { username, onCreateChatRoom } = this.props;
     const { newRoomName, themeColor } = this.state;
 
-    console.log({ username, newRoomName, themeColor, onCreateChatRoom });
-
-    onCreateChatRoom({
-      id: newRoomName,
-      owner: username,
-      themeColor
-    });
+    if (newRoomName.trim().length > 1) {
+      onCreateChatRoom({
+        id: newRoomName,
+        owner: username,
+        themeColor
+      });
+    }
   };
 
   render() {
     return (
       <View style={styles.container}>
-        <CustomInput
-          onChangeText={this.handleRoomNameChange}
-          autoCapitalize="none"
-          placeholder="New Chatroom Name"
+        <View style={styles.fieldWrapper}>
+          <CustomInput
+            onChangeText={this.handleRoomNameChange}
+            autoCapitalize="none"
+            autoCorrect={false}
+            placeholder="New Chatroom Name"
+          />
+        </View>
+        <ColorPicker
+          style={{ flex: 3 }}
+          hideSliders={true}
+          onColorSelected={this.handleColorSelect}
         />
-        <CustomButton
-          title="Create"
-          onPress={this.handleRoomCreate}
-          color="primary"
-        />
+        <View style={styles.fieldWrapper}>
+          <CustomButton
+            title="Create Chatroom"
+            onPress={this.handleRoomCreate}
+            color="primary"
+          />
+        </View>
       </View>
     );
   }
@@ -64,5 +81,9 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     margin: 20
+  },
+  fieldWrapper: {
+    flex: 1,
+    justifyContent: 'center'
   }
 });
