e0318efc397bb1aea70c096258568041a97106ff
Daler Asrorov <asrorids@gmail.com>
2018-10-23 01:43:21 -0700

    remove the padding

--- workshop/components/ChatRoomsFeed.tsx
+++ workshop/components/ChatRoomsFeed.tsx
@@ -79,7 +79,6 @@ export default class ChatRoomsFeed extends React.PureComponent<
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
     justifyContent: 'center',
     backgroundColor: SECONDARY_COLOR
   },
