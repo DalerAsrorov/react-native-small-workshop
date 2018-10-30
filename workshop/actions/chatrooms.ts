40431086daf52ff1d3ee7d8faff594b66e420703
Daler Asrorov <asrorids@gmail.com>
2018-10-30 05:35:40 -0700

    Proper creation load state for chatroom

--- workshop/actions/chatrooms.ts
+++ workshop/actions/chatrooms.ts
@@ -10,13 +10,6 @@ export const requestCreateNewChatRoom = (shouldCreate: boolean = true) => ({
   type: REQUEST_CREATE_NEW_CHATROOM,
   payload: shouldCreate
 });
-
-export const ADD_MY_NEW_CHATROOM_TO_QUEUE = 'ADD_MY_NEW_CHATROOM_TO_QUEUE';
-export const addMyNewChatroomToQueue = (chatroom: ChatRoomProps) => ({
-  type: ADD_MY_NEW_CHATROOM_TO_QUEUE,
-  payload: chatroom
-});
-
 export const createNewChatroom = ({
   name,
   owner,
@@ -25,7 +18,6 @@ export const createNewChatroom = ({
   dispatch(requestCreateNewChatRoom());
 
   return createChatRoom({ name, owner, themeColor }).then(snapshot => {
-    dispatch(addMyNewChatroomToQueue({ name, owner, themeColor }));
     dispatch(fetchAllChatRooms());
     dispatch(requestCreateNewChatRoom(false));
   });
